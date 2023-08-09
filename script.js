1b2b31a6ef19627593f1c094acb144bbba630d52
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-09 07:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+// Given array and object
+var players = [...]; // Your array of players
+var person = {...};  // Your person object
+
+// Create variables to store references and copies
+var team = players;     // Store a reference to the players array
+var team1 = [...players]; // Create a copy of the players array using the spread operator
+var cap1 = {...person};  // Create a copy of the person object using the spread operator
+

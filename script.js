1531e1dc156c9fbba507ee6ae37ac8451be37084
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-09 11:24:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+var team=players;
+var team1=players;
+var cap1=person;
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
@@ -13,11 +16,5 @@ window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
 // Given array and object
-var players = [...]; // Your array of players
-var person = {...};  // Your person object
-
-// Create variables to store references and copies
-var team = players;     // Store a reference to the players array
-var team1 = [...players]; // Create a copy of the players array using the spread operator
-var cap1 = {...person};  // Create a copy of the person object using the spread operator
+  // Create a copy of the person object using the spread operator
 
